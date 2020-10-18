dcac734c4fcebb1667525fad363082f01d45be8f
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-10-18 20:56:11 +0900

    firstcommit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+        Rigidbody2D rigid2D;
+        float jumpForce = 340.0f;
+        float walkForce = 40.0f;
+        float maxWalkSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space)　&& this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        
+        //左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // プレイヤの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1 ,1); 
+        }
+
+        if (transform.position.y < -20)
+        {
+            SceneManager.LoadScene("gameover");
+        }
+    }
+}
